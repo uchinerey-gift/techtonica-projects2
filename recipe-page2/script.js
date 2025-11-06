5808cdd4142d38f2250d9b4a57a22c2d9ec2f22d
Chinerey Ukwu <uchinerey@gmail.com>
2025-11-05 19:33:33 -0700

    added a checkbox to each ingredient list item

--- recipe-page2/script.js
+++ recipe-page2/script.js
@@ -3,3 +3,14 @@ document.addEventListener("DOMContentLoaded", function () {
     console.log("Recipe DOM ready");
 });
 
+const ingredientsList = document.getElementById("ingredients-list");
+const ingredientItems = ingredientsList ? ingredientsList.querySelectorAll("li") : [];
+ingredientItems.forEach((li) => {
+    const box = document.createElement("input");
+    box.type = "checkbox";
+    box.className = "ingredient-check";
+    li.insertBefore(box, li.firstChild);
+    li.insertBefore(document.createTextNode(" "), box.nextSibling);
+});
+
+
